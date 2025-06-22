eb3addfea0750f4393c788b355e7040ac779feb7
prateek <prateek.patel@bricklane.com>
2025-06-22 23:22:53 +0100

    moved from pre to post hook in relevant model

--- models/piper/piper_uk.sql
+++ models/piper/piper_uk.sql
@@ -1,20 +1,3 @@
-{{ config(
-    pre_hook="
-CREATE EXTERNAL TABLE IF NOT EXISTS default.piper_raw (
-  time_period STRING,
-  area_name STRING,
-  rental_price STRING
-)
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
-STORED AS TEXTFILE
-LOCATION 's3://ds-dev-bkt/piper/piper_raw/'
-TBLPROPERTIES (
-  'skip.header.line.count' = '1',
-  'classification' = 'csv'
-)",
-) }}
-
-
 select cast(time_period as date) as time_period
 , area_name
 , cast(rental_price as double) as rental_price
