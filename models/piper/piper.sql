b7a5e43a034a31d3a39273c8282ccaa79476ec46
prateek <prateek.patel@bricklane.com>
2025-06-22 22:08:31 +0100

    using proper destination and source names

--- models/piper/piper.sql
+++ models/piper/piper.sql
@@ -1,4 +1,4 @@
 SELECT cast("Time period" as date) as time_period
 , lower("Area name") as area_name
 , "Rental price" as rental_price
-FROM {{ source("piper", "priceindexofprivaterentsukmonthlypricestatistics.xlsx") }}
+FROM {{ source("priceindexofprivaterentsukmonthlypricestatistics", "priceindexofprivaterentsukmonthlypricestatistics.xlsx") }}
