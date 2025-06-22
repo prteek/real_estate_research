6a414b60e23bbcc1e4b9edcd14bdf347de8a9bad
prateek <prateek.patel@bricklane.com>
2025-06-23 00:11:18 +0100

    moved back ddl to athena side

--- backend/models/piper/piper_raw.sql
+++ backend/models/piper/piper_raw.sql
@@ -1,19 +1,3 @@
-{{ config(
-    post_hook="
-CREATE EXTERNAL TABLE IF NOT EXISTS real_estate.piper_raw (
-  time_period STRING,
-  area_name STRING,
-  rental_price STRING
-)
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
-STORED AS TEXTFILE
-LOCATION 's3://real-estate-research/piper/piper_raw/'
-TBLPROPERTIES (
-  'skip.header.line.count' = '1',
-  'classification' = 'csv'
-)",
-) }}
-
 SELECT cast("Time period" as date) as time_period
 , lower("Area name") as area_name
 , "Rental price" as rental_price
