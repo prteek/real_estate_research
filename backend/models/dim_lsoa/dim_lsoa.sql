03605b70ed27ad28a03b22b068e669bdf3194758
prteek <prteek@icloud.com>
2025-06-24 17:01:57 +0100

    properly converting crs

--- backend/models/dim_lsoa/dim_lsoa.sql
+++ backend/models/dim_lsoa/dim_lsoa.sql
@@ -1,12 +1,12 @@
 select
     lsoa,
     lsoa_name,
-    ST_AsText(geometry) AS lsoa_polygon_wkt,
-    ST_AsText(ST_Centroid(geometry)) AS lsoa_centroid_wkt
+    st_astext(geometry) AS lsoa_polygon_wkt,
+    st_astext(st_centroid(geometry)) AS lsoa_centroid_wkt
 from (
     select
         lower(LSOA21CD) AS lsoa,
         lower(LSOA21NM) AS lsoa_name,
-        ST_Transform(geometry, 'EPSG:4326', 'EPSG:4326') AS geometry  -- Source CRS is already correct
+        st_transform(geometry, 'OGC:CRS84', 'EPSG:4326') AS geometry  -- Source CRS needs converting
     from {{ source('geojsons', 'Lower_layer_Super_Output_Areas_2021_EW_BFC_V8_-8407643096148449625.geojson') }}
 )
--- backend/models/dim_msoa/dim_msoa.sql
+++ backend/models/dim_msoa/dim_msoa.sql
@@ -1,12 +1,12 @@
 select
     msoa,
     msoa_name,
-    ST_AsText(geometry) AS msoa_polygon_wkt,
-    ST_AsText(ST_Centroid(geometry)) AS msoa_centroid_wkt
+    st_astext(geometry) AS msoa_polygon_wkt,
+    st_astext(st_centroid(geometry)) AS msoa_centroid_wkt
 from (
     select
         lower(MSOA21CD) AS msoa,
         lower(MSOA21NM) AS msoa_name,
-        ST_Transform(geom, 'EPSG:4326', 'EPSG:4326') AS geometry  -- Source CRS is already correct
+        st_transform(geometry, 'OGC:CRS84', 'EPSG:4326') AS geometry  -- Source CRS needs conversion
     from {{ source('geojsons', 'MOSA_2021_EW_BFC_V6_8073017919673588333.geojson') }}
 )
