57797bb42a19d89a9b3b1dbcde500f7a3af8185d
prteek <prteek@icloud.com>
2025-06-24 20:31:34 +0100

    using relevant crs with new file

--- backend/models/dim_msoa/dim_msoa.sql
+++ backend/models/dim_msoa/dim_msoa.sql
@@ -7,6 +7,6 @@ from (
     select
         lower(MSOA21CD) AS msoa,
         lower(MSOA21NM) AS msoa_name,
-        st_transform(geometry, 'OGC:CRS84', 'EPSG:4326') AS geometry  -- Source CRS needs conversion
+        st_transform(geom, 'EPSG:27700', 'EPSG:4326') AS geometry  -- Source CRS needs conversion
     from {{ source('geojsons', 'MOSA_2021_EW_BFC_V6_8073017919673588333.geojson') }}
 )
