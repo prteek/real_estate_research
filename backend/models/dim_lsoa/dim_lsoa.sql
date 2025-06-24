2d61f71355c01f4c8487b78a8ec385070c42e6a3
prteek <prteek@icloud.com>
2025-06-24 14:22:00 +0100

    added lsoa and msoa

---
+++ backend/models/dim_lsoa/dim_lsoa.sql
@@ -0,0 +1,12 @@
+select
+    lad,
+    lad_name,
+    ST_AsText(geometry) AS lad_polygon_wkt,
+    ST_AsText(ST_Centroid(geometry)) AS lad_centroid_wkt
+from (
+    select
+        lower(LAD24CD) AS lad,
+        lower(LAD24NM) AS lad_name,
+        ST_Transform(geom, 'EPSG:4326', 'EPSG:4326') AS geometry  -- Source CRS is already correct
+    from {{ source('dim_lad', 'Local_Authority_Districts_May_2024_Boundaries_UK_BFC_-6788913184658251542.geojson') }}
+)
