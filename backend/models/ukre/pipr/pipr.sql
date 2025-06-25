e3e78c900fba33c1784d3873f4e8f0869ac40dae
prteek <prteek@icloud.com>
2025-06-25 18:21:12 +0100

    full pipr dataset

--- backend/models/ukre/pipr/pipr.sql
+++ backend/models/ukre/pipr/pipr.sql
@@ -1,6 +1,14 @@
 SELECT DATE '1899-12-30' + CAST("Time period" AS INTEGER) AS time_period
 , lower("Area name") as area_name
 , case when "Rental price" = '[x]' then null
-else cast("Rental price" as double) end as rental_price
+else cast("Rental price" as double) end as average_price
+, case when "Rental price detached" = '[x]' then null
+else cast("Rental price detached" as double) end as detached_price
+, case when "Rental price semidetached" = '[x]' then null
+else cast("Rental price semidetached" as double) end as semi_detached_price
+, case when "Rental price terraced" = '[x]' then null
+else cast("Rental price terraced" as double) end as terraced_price
+, case when "Rental price flat / maisonette" = '[x]' then null
+else cast("Rental price flat / maisonette" as double) end as flat_price
 FROM {{ source("priceindexofprivaterentsukmonthlypricestatistics", "priceindexofprivaterentsukmonthlypricestatistics.xlsx") }}
 where time_period is not null
