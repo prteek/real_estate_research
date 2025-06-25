be8bb42824ddaf1696cb434119a646adf4384ac4
prteek <prteek@icloud.com>
2025-06-25 10:07:48 +0100

    processing stringy columns

--- backend/models/ukre/pipr/pipr.sql
+++ backend/models/ukre/pipr/pipr.sql
@@ -1,5 +1,6 @@
-SELECT cast("Time period" as date) as time_period
+SELECT DATE '1899-12-30' + CAST("Time period" AS INTEGER) AS time_period
 , lower("Area name") as area_name
-, "Rental price" as rental_price
+, case when "Rental price" = '[x]' then null
+else cast("Rental price" as double) end as rental_price
 FROM {{ source("priceindexofprivaterentsukmonthlypricestatistics", "priceindexofprivaterentsukmonthlypricestatistics.xlsx") }}
 where time_period is not null
